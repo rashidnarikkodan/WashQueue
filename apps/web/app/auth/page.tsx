6281cc6ef9ee633c270ffff32cabf000aecc4d77
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-06-17 14:06:01 +0530

    refactor: remove docs application and configure npm environment settings

--- apps/web/app/auth/page.tsx
+++ apps/web/app/auth/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+"use client"
+import { useState } from "react";
+export default function Auth() {
+  let [count,setCount] = useState(0)
   return (
     <div>
-      auth page
+    <div onClick={()=>setCount(count--)}>-  </div>
+    <div>
+      {count}
+    </div>
+    <div onClick={()=>setCount(count++)}>+</div>
+
     </div>
   );
 }
