3cab6dc8d56d452cb70227efe19e5a455372331c
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-06-25 22:14:18 +0530

    fix: render toaster component in app entry point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes";
 import ThemeProvider from "./shared/providers/ThemeProvider";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
+      <Toaster position="top-right" richColors />
     </ThemeProvider>
   )
 }
