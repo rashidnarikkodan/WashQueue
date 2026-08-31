992bd217cdbc619d8bad84e3633e4c3753c91152
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-08-31 13:41:57 +0530

    feat(transfer):  created razorpay transfer service

--- server/src/core/application/interfaces/payment-gateway.interface.ts
+++ server/src/core/application/interfaces/payment-gateway.interface.ts
@@ -15,3 +15,25 @@ export interface IPaymentGatewayService {
   verifyPaymentSignature(orderId: string, paymentId: string, signature: string): boolean
   verifyWebhookSignature(rawBody: string, signature: string): boolean
 }
+
+
+
+
+// Transfer interface
+export interface CreateTransferParams {
+  amountInPaise: number
+  currency?: string
+  recipientId: string
+  referenceId: string
+}
+
+export interface TransferResult {
+  transferId: string
+  status: "SUCCESS" | "FAILED"
+}
+
+export interface ITransferService {
+  transfer(
+    params: CreateTransferParams
+  ): Promise<TransferResult>
+}
